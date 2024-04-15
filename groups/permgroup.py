9ab9d7fac24bffb7e650994013949fb3b4de5cda
Mateus Marques <mateusmarques2001@usp.br>
2024-04-15 16:42:30 -0300

    Implementando o D3h

--- groups/permgroup.py
+++ groups/permgroup.py
@@ -1,3 +1,6 @@
+class Element:
+    def __init__():
+
 class Perm:
     def __init__(self, L):
         self.n = len(L)
@@ -19,6 +22,28 @@ class Perm:
         print(')\n( ', end='')
         for i in self.perm:
             print(f'{i+1} ', end='')
-        print(')')
+        print(')\n')
 
-D3h =
+def ident(n)
+    return Perm([i for i in range(1, n+1)])
+
+def
+
+def main():
+    E = ident(n)
+    C3 = Perm([3,4,5,6,1,2,7,8])
+    C32 = C3.inv()
+    Sigma_h = Perm([2,1,4,3,6,5,8,7])
+    C21 = Perm([2,1,6,5,4,3,8,7])
+    C22 = Perm([6,5,4,3,2,1,8,7])
+    C23 = Perm([4,3,2,1,6,5,8,7])
+    S3 = C3 * Sigma_h
+    S32 = C32 * Sigma_h
+    Sigma_v1 = C21 * Sigma_h
+    Sigma_v2 = C22 * Sigma_h
+    Sigma_v3 = C23 * Sigma_h
+
+    D3h = [E, C3, C32, C21, C22, C23, Sigma_v1, Sigma_v2, Sigma_v3, Sigma_h, S3, S32]
+
+if __name__ == '__main__':
+    main()
