8749f0d7fe7d002929a5ed3f23fc691759006b30
Mateus Marques <mateusmarques2001@usp.br>
2024-04-20 12:32:22 -0300

    Documentando o codigo no exerc_2 e comentarios

--- groups/permgroup.py
+++ groups/permgroup.py
@@ -1,13 +1,13 @@
-class Group:    # lista de Element
+class Group:    # structure definida como uma lista de 'Element' e o nome do grupo
     def __init__(self, elements, name):
         self.elements = elements
         self.name = name
-    def find(self, perm):
+    def find(self, perm):  # procura o elemento correspondente a uma permutação dada
         for g in self.elements:
             if perm == g.perm:
                 return g
         return None
-    def printtable(self):
+    def printtable(self):  # método que printa a tabela de multiplicação em LaTeX
         elem = self.elements
         group_name = self.name
         order = len(elem)
@@ -17,19 +17,19 @@ class Group:    # lista de Element
                 s = s+"c " if n > 1 else s+"c |"
                 n -= 1
             return s
-        print(r'\begin{tabular} { %s }' % tabular_string(order))
-        print(r'\hline')
+        print(r'\begin{tabular} { %s }' % tabular_string(order)) # tabular do LaTeX
+        print(r'\hline')    # linha horizontal
         print(group_name+' ', end='')
-        for j in range(order):
+        for j in range(order): # printa a primeira linha com os elementos
             if j < order-1:
                 print(f'& {elem[j].eqname} ', end='')
             else:
                 print(f'& {elem[j].eqname} \\\\')
         print(r'\hline')
         for i in range(order):
-            print(f'{elem[i].eqname} & ', end='')
+            print(f'{elem[i].eqname} & ', end='')  # printa a primeira coluna
             for j in range(order):
-                res = self.find(elem[i].perm * elem[j].perm)
+                res = self.find(elem[i].perm * elem[j].perm)  # multiplica elementos
                 if j < order-1:
                     print(f'{res.eqname} & ', end='')
                 else:
@@ -46,19 +46,19 @@ class Element:  # Permutacao e o nome
     def show(self):
         self.perm.show()
 
-class Perm:     # permutacao C3 = Perm([2, 3, 1])
-    def __init__(self, L):
-        self.n = len(L)
-        self.list = [i-1 for i in L]
+class Perm:
+    def __init__(self, L):  # inicialização a partir de uma lista
+        self.n = len(L)     # tamanho da lista
+        self.list = [i-1 for i in L]  # subtrair 1 para indexar a partir do zero
     def __eq__(self, other):
-        return self.list == other.list
-    def __mul__(self, other):
-        return Perm([self.list[i]+1 for i in other.list])
-    def find(self, j):
+        return self.list == other.list  # são iguais se as listas são iguais
+    def __mul__(self, other):  # multiplicação de duas permutações
+        return Perm([self.list[i]+1 for i in other.list])  # composição
+    def find(self, j):  # método auxiliar para achar a posição de um número na lista
         for i in range(self.n):
             if self.list[i] == j:
                 return i
-    def inv(self):
+    def inv(self):  # calcula a inversa de uma permutação
         return Perm([self.find(j)+1 for j in range(self.n)])
     #### DEBUG #####
     def show(self):
@@ -70,48 +70,50 @@ class Perm:     # permutacao C3 = Perm([2, 3, 1])
             print(f'{i+1} ', end='')
         print(')\n')
 
-def ident(n):
+def ident(n):   # elemento identidade
     return Perm([i for i in range(1, n+1)])
 
 def generate_D3h():
     E = Element(ident(8), r'$E$')
     C3 = Element(Perm([3,4,5,6,1,2,7,8]), r'$C_3$')
-    C32 = Element(C3.perm.inv(), r'$C_3^2$')
+    C32 = Element(C3.perm.inv(), r'$C_3^2$')                            # $C_3^{-1}$
     Sigma_h = Element(Perm([2,1,4,3,6,5,8,7]), r'$\sigma_h$')
     C21 = Element(Perm([2,1,6,5,4,3,8,7]), r'$C_2^{(1)}$')
     C22 = Element(Perm([6,5,4,3,2,1,8,7]), r'$C_2^{(2)}$')
     C23 = Element(Perm([4,3,2,1,6,5,8,7]), r'$C_2^{(3)}$')
-    S3 = Element(C3.perm * Sigma_h.perm, r'$S_3$')
-    S32 = Element(C32.perm * Sigma_h.perm, r'$S_3^2$')
-    Sigma_v1 = Element(C21.perm * Sigma_h.perm, r'$\sigma_v^{(1)}$')
-    Sigma_v2 = Element(C22.perm * Sigma_h.perm, r'$\sigma_v^{(2)}$')
-    Sigma_v3 = Element(C23.perm * Sigma_h.perm, r'$\sigma_v^{(3)}$')
-    D3h = Group([E, C3, C32, C21, C22, C23, Sigma_v1, Sigma_v2, Sigma_v3, Sigma_h, S3, S32], r'$D_{3h}$')
+    S3 = Element(C3.perm * Sigma_h.perm, r'$S_3$')                      # $C_3 \sigma_h$
+    S32 = Element(C32.perm * Sigma_h.perm, r'$S_3^2$')                  # $C_3^2 \sigma_h$
+    Sigma_v1 = Element(C21.perm * Sigma_h.perm, r'$\sigma_v^{(1)}$')    # $C_2^{(1)} \sigma_h$
+    Sigma_v2 = Element(C22.perm * Sigma_h.perm, r'$\sigma_v^{(2)}$')    # $C_2^{(2)} \sigma_h$
+    Sigma_v3 = Element(C23.perm * Sigma_h.perm, r'$\sigma_v^{(3)}$')    # $C_2^{(3)} \sigma_h$
+    D3h = Group([E, C3, C32, C21, C22, C23, Sigma_v1,
+                 Sigma_v2, Sigma_v3, Sigma_h, S3, S32], r'$D_{3h}$')
     return D3h
 
 def generate_D3d():
     E = Element(ident(8), r'$E$')
     C3 = Element(Perm([2,3,1,5,6,4,7,8]), r'$C_3$')
-    C32 = Element(C3.perm.inv(), r'$C_3^2$')
-    I = Element(Perm([6,4,5,2,3,1,8,7]), r'$i$')
+    C32 = Element(C3.perm.inv(), r'$C_3^2$')                    # $C_3^{-1}$
     C21 = Element(Perm([6,5,4,3,2,1,8,7]), r'$C_2^{(1)}$')
     C22 = Element(Perm([5,4,6,2,1,3,8,7]), r'$C_2^{(2)}$')
     C23 = Element(Perm([4,6,5,1,3,2,8,7]), r'$C_2^{(3)}$')
-    S6 = Element(C3.perm * I.perm, r'$S_6$')
-    S65 = Element(C32.perm * I.perm, r'$S_6^5$')
-    Sigma_d1 = Element(C21.perm * I.perm, r'$\sigma_d^{(1)}$')
-    Sigma_d2 = Element(C22.perm * I.perm, r'$\sigma_d^{(2)}$')
-    Sigma_d3 = Element(C23.perm * I.perm, r'$\sigma_d^{(3)}$')
-    D3d = Group([E, C3, C32, C21, C22, C23, Sigma_d1, Sigma_d2, Sigma_d3, I, S6, S65], r'$D_{3d}$')
+    I = Element(Perm([6,4,5,2,3,1,8,7]), r'$i$')
+    S6 = Element(C3.perm * I.perm, r'$S_6$')                    # $C_3 i$
+    S65 = Element(C32.perm * I.perm, r'$S_6^5$')                # $C_3^2 i$
+    Sigma_d1 = Element(C21.perm * I.perm, r'$\sigma_d^{(1)}$')  # $C_2^{(1)} i$
+    Sigma_d2 = Element(C22.perm * I.perm, r'$\sigma_d^{(2)}$')  # $C_2^{(2)} i$
+    Sigma_d3 = Element(C23.perm * I.perm, r'$\sigma_d^{(3)}$')  # $C_2^{(3)} i$
+    D3d = Group([E, C3, C32, C21, C22, C23,
+                 Sigma_d1, Sigma_d2, Sigma_d3, I, S6, S65], r'$D_{3d}$')
     return D3d
 
 def conjugacy_classes(group):
-    classes = set()
+    classes = set()     # conjunto de classes
     for g in group.elements:
-        g_class = set()
+        g_class = set() # classe: conjunto de elementos
         for A in group.elements:
-            g_class.add(group.find(A.perm.inv() * g.perm * A.perm))
-        g_class = frozenset(g_class)
+            g_class.add(group.find(A.perm.inv() * g.perm * A.perm)) # $A G A^{-1}$
+        g_class = frozenset(g_class)  # frozenset para criar conjunto de conjuntos
         classes.add(g_class)
     return classes
 
